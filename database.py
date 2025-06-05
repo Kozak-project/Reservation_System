b88e79b7b0b4e4f72eff6ba250102a270df5220b
Kacper Kozak <kozak@MacBookAir.home>
2025-06-05 22:31:32 +0200

    Set database

--- database.py
+++ database.py
@@ -1,42 +1,17 @@
-from sqlalchemy import Column, Integer, String, Numeric, Boolean, DateTime, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_NAME = os.getenv('DB_NAME')
+
+engine = create_engine(f'mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}')
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-class User(Base):
-    __tablename__ = 'user'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    last_name = Column(String(45), nullable=False)
-    email = Column(String(45), nullable=False)
-    phone_number = Column(String(45), nullable=False)
-
-    reservations = relationship('Reservation', back_populates='user')
-
-
-class Room(Base):
-    __tablename__ = 'room'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    room_number = Column(Integer, nullable=False)
-    price_per_night = Column(Numeric(precision=10, scale=2), nullable=False)
-    is_reserved = Column(Boolean, nullable=False)
-    reserved_by = Column(String(45), nullable=True)
-    start_time = Column(DateTime, nullable=True)
-    end_time = Column(DateTime, nullable=True)
-
-    reservations = relationship('Reservation', back_populates='room')
-
-
-class Reservation(Base):
-    __tablename__ = 'reservation'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer,ForeignKey('user.id'), nullable=False)
-    room_id = Column(Integer, ForeignKey('room.id'), nullable=False)
-    start_time = Column(DateTime, nullable=False)
-    end_time = Column(DateTime, nullable=False)
-
-    room = relationship('Room', back_populates='reservations')
-    user = relationship('User', back_populates='reservations')
---
+++ models.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Column, Integer, String, Numeric, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    last_name = Column(String(45), nullable=False)
+    email = Column(String(45), nullable=False)
+    phone_number = Column(String(45), nullable=False)
+
+    reservations = relationship('Reservation', back_populates='user')
+
+
+class Room(Base):
+    __tablename__ = 'room'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    room_number = Column(Integer, nullable=False)
+    price_per_night = Column(Numeric(precision=10, scale=2), nullable=False)
+    is_reserved = Column(Boolean, nullable=False)
+    reserved_by = Column(String(45), nullable=True)
+    start_time = Column(DateTime, nullable=True)
+    end_time = Column(DateTime, nullable=True)
+
+    reservations = relationship('Reservation', back_populates='room')
+
+
+class Reservation(Base):
+    __tablename__ = 'reservation'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer,ForeignKey('user.id'), nullable=False)
+    room_id = Column(Integer, ForeignKey('room.id'), nullable=False)
+    start_time = Column(DateTime, nullable=False)
+    end_time = Column(DateTime, nullable=False)
+
+    room = relationship('Room', back_populates='reservations')
+    user = relationship('User', back_populates='reservations')
