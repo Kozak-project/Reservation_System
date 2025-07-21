9fb14ce75cf7f6f4a3b2b5a1e6cfbcd76d9965b6
Kozak-project <k_kozak@onet.pl>
2025-07-21 18:24:50 +0200

    Added test_all_users

--- tests/test_users.py
+++ tests/test_users.py
@@ -32,3 +32,36 @@ def test_add_user(client, mocker, last_name, email, phone_number):
     mock_session.add.assert_called()
     mock_session.commit.assert_called()
     mock_session.close.assert_called()
+
+
+def test_all_users(client, mocker):
+    mock_users = [
+        mocker.Mock(id=1, last_name='Sam', phone_number=123456789, email='sam@example.com'),
+        mocker.Mock(id=2, last_name='John', phone_number=987654321, email='john@example.com')
+    ]
+
+    mock_session = mocker.Mock()
+    mock_query = mocker.Mock()
+    mock_query.all.return_value = mock_users
+    mock_session.query.return_value = mock_query
+
+    mocker.patch('src.routes.users.SessionLocal', return_value=mock_session)
+
+    response = client.get('/user/all_users')
+
+    assert response.status_code == 200
+    assert response.json == [
+        {
+            'user ID': 1,
+            'user lastname': 'Sam',
+            'user phone number': 123456789,
+            'user e-mail': 'sam@example.com'
+        },
+        {
+            'user ID': 2,
+            'user lastname': 'John',
+            'user phone number': 987654321,
+            'user e-mail': 'john@example.com'
+        }
+    ]
+    
